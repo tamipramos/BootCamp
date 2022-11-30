938db856d07aaa5426138a7e872037c5e3a6afb7
tamipramos <tamipramos@gmail.com>
2022-11-30 12:22:57 +0000

    BootCamp

--- API/index.js
+++ API/index.js
@@ -1,74 +1,69 @@
-//const { response, request } = require('express')
+// const { response, request } = require('express')
 const express = require('express')
 let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true,
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false,
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true,
-    },
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
 ]
-
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (request, response) => {
-    response.send('<h1>aaaaa</h1>')
+  response.send('<h1>aaaaa</h1>')
 })
 
-app.get('/api/notes', (request, response) =>{
-    response.json(notes)
-  
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    if (note)
-    {
-        response.json(note)
-    }
-    else
-    {
-        response.status(404).end()
-    }
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 app.post('/api/notes/', (request, response) => {
-    const note = request.body
-    const ids = notes.map(note => note.id)
-    const maxId = Math.max(...ids)
-    const newNote = {
-        'id': maxId +1,
-        'content': note.content,
-        'important': typeof note.important !== 'undefined' ? note.important : false,
-        'date': new Date().toISOString()
-    }
-    notes = [...notes, newNote]
-    response.json(newNote)
+  const note = request.body
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important: typeof note.important !== 'undefined' ? note.important : false,
+    date: new Date().toISOString()
+  }
+  notes = [...notes, newNote]
+  response.json(newNote)
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Corriendo server en el puerto: [${PORT}]`)
+  console.log(`Corriendo server en el puerto: [${PORT}]`)
 })
